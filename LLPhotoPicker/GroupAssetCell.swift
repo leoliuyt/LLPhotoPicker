906e6aee1e754d9ebb5b861b32dea407eb5a891c
leoliu <leoliuyt@sina.cn>
2016-12-06 22:07:07 +0800

    group cell

---
+++ LLPhotoPicker/GroupAssetCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroupAssetCell.swift
+//  LLPhotoPicker
+//
+//  Created by leoliu on 2016/12/6.
+//  Copyright © 2016年 LL. All rights reserved.
+//
+
+import UIKit
+
+class GroupAssetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
